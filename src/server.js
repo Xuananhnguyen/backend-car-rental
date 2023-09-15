500f6d8c74b437402a7d2117d045c8fe8245866d
Xuananhnguyen <xuananhnguyen0306@gmail.com>
2023-09-16 01:02:41 +0700

    format code

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
-
+const connection = require('./config/database');
 
 const app = express();// app express
 const port = process.env.PORT || 8888; // port
@@ -15,20 +14,10 @@ configViewEngine(app)
 // Khai báo route
 app.use('/', webRoutes);
 
-//test connection 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, //default: 3306
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-});
-
 connection.query(
     'SELECT * FROM Users',
     function (err, results, fields) {
         console.log(">>> results=", results); // results contains rows returned by server
-        console.log(">>> fields=", fields); // fields contains extra meta data about results, if available
     }
 );
 
